971af98ffdc42d90cf98d92ab2cd7d502da1756b
Paul Michali <pc@michali.net>
2024-05-14 20:53:55 -0400

    Customizations for working on ARM64, and with newer K8s.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # K8s-OpenVPN
 
-Repository contains information on how to build, install and configure OpenVPN for kubernetes cluster.
+Repository contains information on how to build, install and configure OpenVPN for kubernetes cluster, customized to work with ARM64 hosts and updated for newer K8s.
 
-Inspired by
+Forked from
+- https://github.com/k4kratik/k8s-openvpn.git
+
+Which was Inspired by
 
 - <https://github.com/kylemanna/docker-openvpn>
 - <https://github.com/helm/charts/tree/master/stable/openvpn>
--- manage/revoke_user.sh
+++ manage/revoke_user.sh
@@ -2,12 +2,12 @@
 
 if [ $# -ne 1 ]
 then
-  echo "Usage: $0 <CLIENT_KEY_NAME> <NAMESPACE> <HELM_RELEASE>"
+  echo "Usage: $0 <CLIENT_KEY_NAME>"
   exit
 fi
 
 KEY_NAME=$1
-NAMESPACE=kratik
+NAMESPACE=k8s-openvpn
 HELM_RELEASE=openvpn
 POD_NAME=$(kubectl get pods -n "$NAMESPACE" -l "app=openvpn,release=$HELM_RELEASE" -o jsonpath='{.items[0].metadata.name}')
-kubectl -n "$NAMESPACE" exec -it "$POD_NAME" /etc/openvpn/setup/revokeClientCert.sh $KEY_NAME
+kubectl -n "$NAMESPACE" exec -it "$POD_NAME" -- /etc/openvpn/setup/revokeClientCert.sh $KEY_NAME
